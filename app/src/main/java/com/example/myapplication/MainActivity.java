4d8c0d1c7aec38a809e3dcfc35839d820cccf965
JuniorTheF <fsmart2006@gmail.com>
2023-01-17 20:32:40 +0300

    OMG ITS WORKING

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.icu.math.BigDecimal;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button one = findViewById(R.id.btn1);
@@ -56,13 +60,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         equals.setOnClickListener(this);
         Button dot = findViewById(R.id.btndot);
         dot.setOnClickListener(this);
-        Button negative = findViewById(R.id.btnneg);
-        negative.setOnClickListener(this);
+        ImageView git = findViewById(R.id.btnneg);
+        git.setOnClickListener(view -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/JuniorTheF"))));
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onClick(View view) {
+        String flag = "";
         TextView textView = findViewById(R.id.textView);
         Button b = (Button) view;
         String buttonText = b.getText().toString();
@@ -85,7 +91,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btndivide:
             case R.id.btnpercent:
             case R.id.btndot:
-                sanityCheck();
+//                textView.append(buttonText);
+                flag = "operation";
+                sanityCheck(flag, view);
                 break;
             case R.id.btndel:
                 if (textView.getText().toString().length() != 0) {
@@ -98,43 +106,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.btnequals:
-
-                solve(createNumbers());
-                break;
-            case R.id.btnneg:
-                // do your code
+                flag = "equals";
+                sanityCheck(flag, view);
                 break;
             default:
                 break;
         }
     }
 
-    public void sanityCheck() {
-        //TODO
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void sanityCheck(String event, View view) {
+        TextView textView = findViewById(R.id.textView);
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+        if (event.equals("equals")) {
+            if ((textView.getText().toString().contains("-") || textView.getText().toString().contains("+") || textView.getText().toString().contains("*") || textView.getText().toString().contains("/") || textView.getText().toString().contains("^")) && (!textView.getText().toString().endsWith("-") && !textView.getText().toString().endsWith("+") && !textView.getText().toString().endsWith("*") && !textView.getText().toString().endsWith("/") && !textView.getText().toString().endsWith("^"))) {
+            solve(createNumbers());
+        }
+            else {
+                textView.setText("incorrect equation");
+            }
+        }
+        else {
+           if ((textView.getText().toString().endsWith("-") || textView.getText().toString().endsWith("+") || textView.getText().toString().endsWith("*") || textView.getText().toString().endsWith("/") || textView.getText().toString().endsWith("^") || textView.getText().toString().endsWith("."))) {
+               textView.setText(textView.getText().toString().substring(0, textView.getText().toString().length()-1) + buttonText);
+           }
+           else {
+               textView.append(buttonText);
+           }
+//           else {
+//               textView.setText(buttonText);
+//           }
+        }
+
     }
 
     public ArrayList<Object> createNumbers() {
         TextView textView = findViewById(R.id.textView);
         String temp = textView.getText().toString();
         ArrayList<Integer> digits = new ArrayList<>();
+        String letters = "qwertyuiopasdfghjklzxcvbnm";
         int intnumber = 0;
-//        float div = 0;
-//        float doublenumber = 0;
         ArrayList<Object> equation = new ArrayList<>();
+        for (char k : letters.toCharArray()) {
+            if (temp.contains(String.valueOf(k))) {
+                equation.add("char error");
+                return equation;
+            }
+        }
         for (int i = 0; i < temp.length(); i++) {
             if (temp.charAt(i) != '-' && temp.charAt(i) != '+' && temp.charAt(i) != '*' && temp.charAt(i) != '/' && temp.charAt(i) != '^' && temp.charAt(i) != '.') {
-//                    if (temp.charAt(i) == ',') {
-//                        div = (float) Math.pow(0.1, i);
-//                    }
-//                    else {
                         digits.add(Integer.parseInt(String.valueOf(temp.charAt(i))));
                         if (i + 1 == temp.length()) {
                             Integer[] ardigits = digits.toArray(new Integer[0]);
                             for (int k = 0; k < ardigits.length; k++) {
                                 intnumber = intnumber * 10 + ardigits[k];
                             }
-//                            if (div == 0) {
-//                                System.out.println(intnumber);
                                 equation.add(intnumber);
                                 intnumber = 0;
                                 digits.clear();
@@ -148,14 +175,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                            }
                         }
                     }
-//                }
-
             else {
                 Integer[] ardigits = digits.toArray(new Integer[0]);
                 for (Integer ardigit : ardigits) {
                     intnumber = intnumber * 10 + ardigit;
                 }
-//                System.out.println(intnumber);
                 equation.add(intnumber);
                 intnumber = 0;
                 digits.clear();
@@ -189,7 +213,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 public void solve(ArrayList<Object> numbers) {
         TextView textView = findViewById(R.id.textView);
         System.out.println("eq " + numbers);
-
+        if (numbers.contains("char error")) {
+            textView.setText("char error");
+            return;
+        }
         while (numbers.contains('.')) {
             int dot = numbers.indexOf('.');
             double number = ((int) numbers.get(dot-1) + Math.pow(0.1, numbers.get(dot+1).toString().length()) * (int) numbers.get(dot+1));
@@ -201,7 +228,7 @@ public void solve(ArrayList<Object> numbers) {
 
         while (numbers.contains('^')) {
             int pow = numbers.indexOf('^');
-            BigDecimal number = BigDecimal.valueOf(Math.pow(Double.parseDouble(numbers.get(pow-1).toString()), Double.parseDouble(numbers.get(pow+1).toString()))).setScale(6, 4);
+            BigDecimal number = BigDecimal.valueOf(Math.pow(Double.parseDouble(numbers.get(pow-1).toString()), Double.parseDouble(numbers.get(pow+1).toString()))).setScale(7, 6);
             numbers.set(pow-1, number);
             numbers.remove(pow);
             numbers.remove(pow);
@@ -209,24 +236,30 @@ public void solve(ArrayList<Object> numbers) {
         System.out.println("pow " + numbers);
 
     while (numbers.contains('*') || numbers.contains('/')) {
+        System.out.println("muldiv1 " + numbers);
         int mult = numbers.indexOf('*');
         int div = numbers.indexOf('/');
         if (div == -1 || (mult < div && mult != -1)) {
-            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(mult-1).toString()) * (Double.parseDouble(numbers.get(mult+1).toString()))).setScale(6, 4) ;
+            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(mult-1).toString()) * (Double.parseDouble(numbers.get(mult+1).toString()))).setScale(7, 6) ;
             numbers.set(mult-1, number);
             numbers.remove(mult);
             numbers.remove(mult);
         }
         else if (mult > div || mult == -1) {
-            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(div-1).toString()) / (Double.parseDouble(numbers.get(div+1).toString()))).setScale(6, 4);
-            numbers.set(div-1, number);
-            numbers.remove(div);
-            numbers.remove(div);
+            System.out.println("muldiv2 " + numbers);
+            if (Double.parseDouble(numbers.get(div+1).toString()) != 0.0) {
+                System.out.println("muldiv3 " + numbers);
+                BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(div-1).toString()) / (Double.parseDouble(numbers.get(div+1).toString()))).setScale(7, 6);
+                numbers.set(div-1, number);
+                numbers.remove(div);
+                numbers.remove(div);
+            }
+            else {
+                System.out.println("muldiv4 " + numbers);
+                textView.setText("incorrect equation");
+                return;
+            }
         }
-//        BigDecimal number = BigDecimal.valueOf(Math.pow(Double.parseDouble(numbers.get(pow-1).toString()), Double.parseDouble(numbers.get(pow+1).toString()))).setScale(3, 2);
-//        numbers.set(pow-1, number);
-//        numbers.remove(pow);
-//        numbers.remove(pow);
     }
     System.out.println("muldiv " + numbers);
 
@@ -234,13 +267,13 @@ public void solve(ArrayList<Object> numbers) {
         int plus = numbers.indexOf('+');
         int minus = numbers.indexOf('-');
         if (plus == -1 || (minus < plus && minus != -1) ) {
-            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(minus-1).toString()) - (Double.parseDouble(numbers.get(minus+1).toString()))).setScale(6, 4);
+            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(minus-1).toString()) - (Double.parseDouble(numbers.get(minus+1).toString()))).setScale(7, 6);
             numbers.set(minus-1, number);
             numbers.remove(minus);
             numbers.remove(minus);
         }
         else if (minus > plus || minus == -1) {
-            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(plus-1).toString()) + (Double.parseDouble(numbers.get(plus+1).toString()))).setScale(6, 4);
+            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(numbers.get(plus-1).toString()) + (Double.parseDouble(numbers.get(plus+1).toString()))).setScale(7, 6);
             numbers.set(plus-1, number);
             numbers.remove(plus);
             numbers.remove(plus);
@@ -253,11 +286,11 @@ public void solve(ArrayList<Object> numbers) {
     if (result.compareTo(BigDecimal.valueOf(res)) == 0) {
         int intresult = result.intValue();
         textView.setText(getString(R.string._text));
-        textView.append(String.valueOf(Math.round(intresult)));
+        textView.append(String.valueOf(intresult));
     }
     else {
         textView.setText(getString(R.string._text));
-        textView.append(String.valueOf((Math.round(result.floatValue()))));
+        textView.append(String.valueOf((result.floatValue())));
     }
 
 }
